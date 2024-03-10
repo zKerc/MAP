bd5e7f719ce4a48a4e9bc5d1918302b1eb2128f7
Arthur Pereira da Silva <129605534+Arthur-Pereira-da-Silva@users.noreply.github.com>
2024-03-10 16:13:25 -0300

    Update Professor.java

--- Lab1/src/Professor.java
+++ Lab1/src/Professor.java
@@ -1,37 +1,25 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Professor {
-    private String nome;
-    private List<Disciplina> disciplinas;
-    private String horario;
+	 private String nome;
+	    private List<Disciplina> disciplinasAtuais;
 
-    public Professor(String nome, List<Disciplina> disciplinas, String horario) {
-        this.nome = nome;
-        this.disciplinas = disciplinas;
-        this.horario = horario;
-    }
+	    public Professor(String nome) {
+	        this.nome = nome;
+	        this.disciplinasAtuais = new ArrayList<>();
+	    }
 
-    public String getNome() {
-        return nome;
-    }
+	    public String getNome() {
+	        return nome;
+	    }
+	    //Quais disciplinas um professor está ministrando?
+	    public List<Disciplina> getDisciplinasMinistradas() {
+	        return disciplinasAtuais;
+	    }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public List<Disciplina> getDisciplinas() {
-        return disciplinas;
-    }
-
-    public void setDisciplinas(List<Disciplina> disciplinas) {
-        this.disciplinas = disciplinas;
-    }
-
-    public String getHorario() {
-        return horario;
-    }
-
-    public void setHorario(String horario) {
-        this.horario = horario;
-    }
+	    public void atribuirDisciplina(Disciplina disciplina) {
+	        disciplinasAtuais.add(disciplina);
+	        disciplina.setProfessorTitular(this);
+	    }
 }
