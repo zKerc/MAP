87bc122033b277b2085cfb0436b26d5e72739e99
Arthur Pereira da Silva <129605534+Arthur-Pereira-da-Silva@users.noreply.github.com>
2024-03-11 17:34:32 -0300

    Update Disciplina.java

--- Lab1/src/Disciplina.java
+++ Lab1/src/Disciplina.java
@@ -42,7 +42,7 @@ public class Disciplina {
         }
 
         alunosMatriculados.add(aluno);
-        aluno.matricularDisciplina(this);
+        aluno.matricularDisciplina(this); //Passa a disciplina como argumento para atualizar as disciplinas do aluno
         return true;
     }
 
